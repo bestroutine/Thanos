dd4f270bb1ac94e935fc8a16f636a227f758fc14
dzhang <dzhang@hillinsight.com>
2018-09-06 15:39:53 +0800

    图片弹窗

--- screens/content/GoodList.js
+++ screens/content/GoodList.js
@@ -1,11 +1,51 @@
 import React, {Component} from 'react';
-import {StyleSheet,View,Text,TextInput,Image} from 'react-native';
+import {StyleSheet,View,Text,TextInput,Image, Alert, TouchableOpacity} from 'react-native';
 import { setFont, setSize } from "../../utils/resolution";
+import BigGoodsImage from './BigGoodsImage';
+const imgLocation = require('../../assets/images/pages/img-location.png');
+
 
 export default class GoodList extends Component {
 	constructor(props) {
   	super(props);
+    this.state = {
+      bigGoodsShow: false,
+      bigGoodsShowData: {},
+    }
 	}
+
+  _stockList(flag){
+    if(flag == 0){
+      return(
+        <View style={styles.good_null}>
+          <Text style={styles.null_text}>已售磬</Text>
+        </View>
+      )
+    }
+  }
+
+  _buttonBuy(flag){
+    if(flag == 0){
+      return(
+        <Text style={styles.buy_text_no}>购买</Text>
+      )
+    }else{
+      return(
+        <Text style={styles.buy_text} onPress={()=>this.showSelectModule()}>购买</Text>
+      )
+    }
+  }
+
+  showSelectModule(){
+    Alert.alert('购买弹窗')
+  }
+
+  showBigGoods(item){
+    this.setState({
+      bigGoodsShow: true,
+      bigGoodsShowData: item,
+    })
+  }
 
 	render() {
     const goodList= this.props.goodList
@@ -29,9 +69,12 @@ export default class GoodList extends Component {
         resarr.push(
           <View style={styles.good} key={item.tag_id}>
             <View styles={styles.image}>
-              <Image 
-              style={styles.good_img} 
-              source={{uri:item.img_optimize_path}} />
+              <TouchableOpacity  onPress={()=>this.showBigGoods(item)}>
+                <Image 
+                style={styles.good_img} 
+                source={item.img_optimize_path?{uri:item.img_optimize_path}:imgLocation} />
+                {this._stockList(item.commodity_stock)}
+              </TouchableOpacity>
             </View>
             <View style={styles.good_msg}>
               <Text 
@@ -44,13 +87,18 @@ export default class GoodList extends Component {
               <View style={styles.buy}>
                 <Text style={styles.buy_price}>¥{parseInt(item.sale_price)}</Text>
                 <View style={styles.buy_button}>
-                  <Text style={styles.buy_text}>购买</Text>
+                  {this._buttonBuy(item.commodity_stock)}
                 </View>
               </View>
             </View>
           </View>
         )
       })}
+      if(goodData.length>0){
+        resarr.push(
+          <BigGoodsImage key={goodData.length} goodsData={this.state.bigGoodsShowData} goodVisible={this.state.bigGoodsShow}/>
+        )
+      }
       return resarr
     }
     return(
@@ -61,21 +109,35 @@ export default class GoodList extends Component {
 
 const styles = StyleSheet.create({
   good:{
-    marginBottom: setSize(40),
     marginTop: setSize(40),
     flexDirection: "row",
   },
-  good_image:{
-    width: setSize(160),
-    height: setSize(160),
-    borderRadius: setSize(8),
-    
-    backgroundColor:'#f8f8f8'
+  image:{
+    position: 'relative',
   },
   good_img:{
     width: setSize(160),
     height: setSize(160),
     marginRight:setSize(20),
+  },
+  good_null:{
+    position:'absolute',
+    left: 0,
+    top: 0,
+    width: setSize(160),
+    height: setSize(160),
+    backgroundColor: 'rgba(255,255,255,.6)'
+  },
+  null_text:{
+    position: 'absolute',
+    left: setSize(29),
+    top: setSize(57),
+    color: '#333',
+    fontSize:setFont(28),
+    padding: setSize(9),
+    borderWidth:1,
+    borderColor: '#333',
+    borderRadius: setSize(4)
   },
   good_msg:{
     flex:1,
@@ -98,7 +160,7 @@ const styles = StyleSheet.create({
   buy_button:{
     alignItems: 'center',
   },
-  buy_text:{
+  buy_text_no:{
     width:setSize(144),
     height:setSize(50),
     borderRadius:setSize(4),
@@ -106,6 +168,16 @@ const styles = StyleSheet.create({
     borderColor: '#ddd',
     textAlign:'center',
     color: '#999',
+    lineHeight:setSize(45)
+  },
+  buy_text:{
+    width:setSize(144),
+    height:setSize(50),
+    borderRadius:setSize(4),
+    borderWidth:1,
+    borderColor: '#00BBB4',
+    textAlign:'center',
+    color: '#00BBB4',
     lineHeight:setSize(45)
   }
 })
--- screens/content/Swiper.js
+++ screens/content/Swiper.js
@@ -1,39 +1,60 @@
 import React, {Component} from 'react';
 import {StyleSheet,View,Text,TextInput,Image} from 'react-native';
-import { setFont, setSize } from "../../utils/resolution";
+import { setFont, setSize, screenW} from "../../utils/resolution";
 import Swiper from 'react-native-swiper';
 
-// <View style={styles.slide} >
-//               <Text numberOfLines={1}>Aussie tourist dies at Bali hotel</Text>
-//               <Image resizeMode='stretch' style={styles.image} source={require('../../assets/images/pages/checked.png')} />
-//           </View>
-//           <View style={styles.slide}>
-//               <Text numberOfLines={1}>Big lie behind Nine’s new show</Text>
-//               <Image resizeMode='stretch' style={styles.image} source={require('../../assets/images/pages/heart.png')} />
-//           </View>
-//           <View style={styles.slide} >
-//               <Text numberOfLines={1}>Why Stone split from Garfield</Text>
-//               <Image resizeMode='stretch' style={styles.image} source={require('../../assets/images/pages/my.png')} />
-//           </View>
-//           <View style={styles.slide}>
-//               <Text numberOfLines={1}>Learn from Kim K to land that job</Text>
-//               <Image resizeMode='stretch' style={styles.image} source={require('../../assets/images/pages/mine.png')} />
-//           </View>
 
 export default class Content extends Component {
 	constructor(props) {
   	super(props);
-  	
 	}
 
   componentWillMount() {
   	
   }
 
+  
+
 	render() {
     // console.log(this.props.detailPic)
     // console.log('---------')
     const picDetail = this.props.detailPic; //图片内容
+    let DomArr = []
+    picDetail.forEach((item,idx)=>{
+      let tagListData = item.tag ||[]
+      DomArr.push(
+        <View style={styles.slide} key={idx}>
+          if(item.tag&&item.tag.length>0){
+            tagListData.map((tip,i)=>{
+              // console.log(tip)
+              return(
+                <View key={i} style={[
+                    styles.tags,
+                    {
+                      position: 'absolute',
+                      left: tip.location_x*screenW,
+                      top: tip.location_y*screenW
+                    }
+                  ]}
+                > 
+                  <View style={styles.tag_circle}>
+                    <Text style={styles.min_circle}></Text>
+                    <Text style={styles.max_circle}></Text>
+                  </View>
+                  <View style={styles.tag_line}></View>
+                  <View style={styles.tag}>
+                    <Text style={styles.swiper_tag}>
+                      {parseInt(tip.tag_price)>0? '¥'+parseInt(tip.tag_price)+' ': ''}{tip.tag_name}
+                    </Text>
+                  </View>
+                </View>
+              )
+            })
+          }
+          <Image resizeMode='contain' style={styles.image} source={{uri: item.pic_optimize_url}} />
+        </View>
+      )
+    })
     return (
     	<View 
         style={{
@@ -48,29 +69,14 @@ export default class Content extends Component {
           dot={<View style={{backgroundColor:'rgba(218,218,218,1)', width: setSize(12), height: setSize(12),borderRadius: 2, marginLeft: 3, marginRight: 3, marginTop: 3, marginBottom: 3,}} />}
           activeDot={<View style={{backgroundColor: '#000', width: setSize(12), height: setSize(12), borderRadius: 2, marginLeft: 3, marginRight: 3, marginTop: 3, marginBottom: 3}} />}
           paginationStyle={{
-              bottom: 23, left: 0, right: 0
+              bottom: setSize(20), left: 0, right: 0
           }}
           loop
+          autoplay={false}
+          key={this.props.detailPic.length}
         >
-          
-
-          {picDetail.map((item, idx)=>{
-
-            return(
-
-              <View style={styles.slide} key={idx}>
-
-                <Text numberOfLines={1}>Aussie tourist dies at Bali hotel</Text>
-                <Image resizeMode='stretch' style={styles.image} source={{uri: item.pic_optimize_url}} />
-
-              </View>
-
-            );
-
-          })}
-
+          { DomArr}
         </Swiper>
-
     	</View>
     );
 	}
@@ -88,25 +94,57 @@ const styles = StyleSheet.create({
       flex: 1,
       justifyContent: 'center',
       alignItems: 'center',
-      backgroundColor: 'transparent'
+      backgroundColor: 'transparent',
+      position: 'relative'
     },
-
-    slide1: {
-      flex: 1,
-      justifyContent: 'center',
+    tags:{
+      flexDirection: "row",
       alignItems: 'center',
-      backgroundColor: '#9DD6EB'
+      zIndex: 2,
     },
-
-    text: {
+    tag_circle:{
+      position: 'relative'
+    },
+    min_circle:{
+      position: 'absolute',
+      left: setSize(6.5),
+      top: setSize(6.5),
+      width:setSize(10),
+      height: setSize(10),
+      backgroundColor: '#fff',
+      borderRadius: setSize(20),
+      zIndex:2,
+    },
+    max_circle:{
+      width:setSize(22),
+      height: setSize(22),
+      backgroundColor: 'rgba(0,0,0,.6)',
+      borderRadius: setSize(22),
+    },
+    tag_line:{
+      width:setSize(35),
+      borderWidth: 0,
+      borderBottomWidth: 2,
+      borderColor: 'rgba(0,0,0,.7)',
+    },
+    tag:{
+      paddingBottom: setSize(12),
+      paddingLeft: setSize(15),
+      paddingRight: setSize(15),
+      backgroundColor: 'rgba(0,0,0,.7)',
+      borderRadius: setSize(4),
+    },
+    swiper_tag: {
       color: '#fff',
-      fontSize: 30,
-      fontWeight: 'bold'
+      fontSize: setFont(24),
+      fontWeight: 'bold',
+      paddingTop: setSize(12),
+      
     },
 
     image: {
-      width:100,
-      height:100
+      width:'100%',
+      height:'100%'
     }
 });
 
